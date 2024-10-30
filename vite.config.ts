1439bd2d73f5f83769a8d35d9ccb014c6ae57303
Kahfi <kahfi@zero-one-group.com>
2024-10-30 12:57:49 +0700

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'kahfi-tailwind',
   plugins: [react()],
   build: {
     emptyOutDir: true,
